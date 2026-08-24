57eed3be704f22b9c10a44232f249d96dced8f6b
murer <paulo.murer@infinity6.ai>
2026-08-24 17:35:39 -0300

    x

---
+++ releases/sdk-web/v0.0.1/i6sdk.js
@@ -0,0 +1,2 @@
+(()=>{var t=class{async init(i){this.config=i}async predictBoughtTogether(i){return i}};var n=class{async init(i){if(this.config=i,!this.config)throw new Error("SDK configuration is missing.")}async RaasBoughtTogether(i){let r={...this.config,...i},o=new t;return await o.init(r),o}};window.I6Sdk=n;})();
+//# sourceMappingURL=sdk.min.js.map
