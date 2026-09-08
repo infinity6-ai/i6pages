77a825756ce556b7f790b9ffa24f4aa5de9e8784
murer <paulo.murer@infinity6.ai>
2026-09-08 12:06:04 -0300

    x

--- leo-helloworld-sdk/leo-sdk.js
+++ leo-helloworld-sdk/leo-sdk.js
@@ -9,6 +9,14 @@
             })
         }
 
+        async sum(a, b) {
+            return new Promise(resolve => {
+                const loc = '' + location.href
+                const result = a + b
+                setTimeout(() => resolve(`sum leosdk: ${result}, location: ${location.href}`), 2000)
+            })
+        }
+
     }
 
     window.leosdk = new LeoSdk()
